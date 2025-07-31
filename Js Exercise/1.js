0e6cb0f6cfc9399b4687518d8a25e5ebf4a15e54
Hrinal Meshram <127570861+Rinalm@users.noreply.github.com>
2025-07-31 21:48:19 +0530

    Add files via upload

---
+++ Js Exercise/1.js
@@ -0,0 +1,79 @@
+// Q1. print in console
+
+// console.log('hello bro')
+
+// m2:
+// let a= 'hewlllo baba';
+// console.log(a)
+
+///=========================================================================
+
+///Q2. print the no. from 1 to 5 using the loop 
+// //M1; by for loop' 
+// for(let i=1; i<6; i+=2){
+//     console.log(i)
+// }
+
+//m2: by while
+// let i=1; // init
+// while(i<6){
+//     console.log(i);
+//     i++;
+// }
+
+//m3: implement , for in & for of loop ???
+
+/////=========================================================================
+
+// Q3. write a function to add two no.s and return the ans 
+// function addkr(a,b){
+//     return a+b
+// }
+
+// console.log(addkr(3,6))/// funtn call inside the print line]
+// ///or
+// let sum = addkr(3,6);
+// console.log(sum)
+
+/////=========================================================================
+
+// Q4. create a function to find the area of a rectangle given its width and height?
+// area = l*b
+// M1;
+// function area(a,b){
+//     return a*b
+// }
+// M2; by arrow funciton: replace {} , return with =>
+// let area = (a,b) =>   a*b ;
+// console.log(area(4,5))
+// console.log('ye hai area : ' + area(4,5)) // str + value 
+
+
+//=============================String Manipulation============================================
+
+// Q5.. rev the string by functoin
+// //  write a function that takes a str and return the reverse of it
+// let str = 'helllo babaua kaisn ba '
+// function revStr(str){
+//     return str.split("").reverse().join("") // break , rev then joinning the str
+// }
+// console.log(revStr(str))
+
+///===========================================================================================
+// conditional\
+// Q6. write a function that checks , if a no. is even or odd  returns 'even' or 'odd'let\
+// // let checkit
+// function checkit(n){
+//     if(n%2==0) 
+//         return 'even';
+//     else
+//         return 'odd';
+// }
+// console.log(checkit(4))
+
+///===========================================================================================
+
+// find leap year by functioln
+///===========================================================================================
+
+///===========================================================================================
